30c6106bb20be1a10518c5d00955001d5000e2d3
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2017-10-01 16:45:46 -0600

    objects added

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-var a = 0;
-var b = false;
+var person = new Object();
 
-if(a === b){
-  console.log('They are equal');
+person["firstname"] = "Tony";
+person["lastname"] = "Alicea";
 
-} else {
-  console.log('Nope, not equal');
-}
+var firstNameProperty = "firstname";
+
+console.log(person);
+console.log(person[firstNameProperty]);
+
+console.log(person.firstname);
+console.log(person.lastname);
+
+person.address = new Object();
+person.address.street = "111 Main St.";
+person.address.city = "New York";
+person.address.state = "NY";
+
+console.log(person.address.street);
+console.log(person.address.city);
+console.log(person["address"]["state"]);
