022946c0ecc8a48fd406741be4918b5b7be0e379
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2017-10-09 19:29:18 -0600

    array practice

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
-greet();
+var arr = [
+    1,
+    false,
+    {
+        name: 'Jason',
+        address: '345 main'
+    },
+    function (name) {
+        var greeting = 'Hello ';
+        console.log(greeting + name);
+    },
+    'hello'
+];
 
-function greet() {
-    console.log('hi');
+console.log(arr);
+arr[3](arr[2].name);
 
-}
-
-
-var annonymousGreet = function () {
-    console.log('hi');
-}
-
-annonymousGreet();
-
-
-function log(a) {
-    a();
-}
-
-log(function () {
-    console.log('hi');
-});
+// greet();
+//
+// function greet() {
+//     console.log('hi');
+//
+// }
+//
+//
+// var annonymousGreet = function () {
+//     console.log('hi');
+// }
+//
+// annonymousGreet();
+//
+//
+// function log(a) {
+//     a();
+// }
+//
+// log(function () {
+//     console.log('hi');
+// });
 //greet.language = 'english';
 //
 //console.log(greet.language);
